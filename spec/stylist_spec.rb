3e8c9cf612e9d7e7f31b659ecbe99e3a5f15c1b7
Duane M Moody <dmmoody@gmail.com>
2015-01-23 12:35:18 -0800

    add #clients spec

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -48,4 +48,16 @@ describe(Stylist) do
     end
   end
 
+  describe("#clients") do
+    it("returns an array of clients for that stylist") do
+      test_stylist = Stylist.new({:name => "Cierra", :id => nil})
+      test_stylist.save()
+      test_client = Client.new({:name => "Duane", :id => nil, :stylist_id => test_stylist.id()})
+      test_client.save()
+      test_client2 = Client.new({:name => "Steve", :id => nil, :stylist_id => test_stylist.id()})
+      test_client2.save()
+      expect(test_stylist.clients()).to(eq([test_client, test_client2]))
+    end
+  end
+
 end
