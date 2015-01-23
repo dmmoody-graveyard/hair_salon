b2c451fb01e3840c5645b9ac08befc25eb177cd0
Duane M Moody <dmmoody@gmail.com>
2015-01-23 10:18:29 -0800

    correct attributes in #name spec

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -4,7 +4,7 @@ describe(Stylist) do
 
   describe('#name') do
     it('returns a stylist name') do
-      test_stylist = Stylist.new("Cierra")
+      test_stylist = Stylist.new(:name => "Cierra", :id => nil)
       expect(test_stylist.name()).to(eq("Cierra"))
     end
   end
