00b92c404383ff63fc1d3baf45e33bb894441342
Duane M Moody <dmmoody@gmail.com>
2015-01-23 11:08:56 -0800

    add #id spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,11 +1,18 @@
 require('spec_helper')
 
 describe(Client) do
-	
+  
   describe('#name') do
     it('returns a client name') do
-      test_client = Client.net(:name => "Duane", :id => nil)
+      test_client = Client.new(:name => "Duane", :id => nil)
       expect(test_client.name()).to(eq("Duane"))
+    end
+  end
+
+  describe('#id') do
+    it('returns a client id') do
+      test_client = Client.new(:name => "Duane", :id => nil)
+      expect(test_client.id()).to(eq(nil))
     end
   end
 
