403a5d1423eeb5c502d0aa4d29affb2c0433c062
Duane M Moody <dmmoody@gmail.com>
2015-01-23 10:44:13 -0800

    add .all #save #==

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -7,4 +7,23 @@ class Stylist
     @id = attributes.fetch(:id)
   end
 
+  define_singleton_method(:all) do
+    returned_tasks = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_tasks.each() do |stylist|
+      name = stylist.fetch("name")
+      id = stylist.fetch("id")
+      stylists.push(Stylist.new({:name => name, :id => id}))
+    end
+    stylists
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}');")
+  end
+
+  define_method(:==) do |another_stylist|
+    self.name().==(another_stylist.name())
+  end
+
 end
