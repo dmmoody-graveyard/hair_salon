8b5a828a2c69336004405decb22e6b1df3e47978
Duane M Moody <dmmoody@gmail.com>
2015-01-23 11:41:44 -0800

    add #save #==

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,21 +4,44 @@ describe(Client) do
   
   describe('#name') do
     it('returns a client name') do
-      test_client = Client.new(:name => "Duane", :id => nil)
+      test_client = Client.new(:name => "Duane", :id => nil, :stylist_id => 1)
       expect(test_client.name()).to(eq("Duane"))
     end
   end
 
   describe('#id') do
     it('returns a client id') do
-      test_client = Client.new(:name => "Duane", :id => nil)
+      test_client = Client.new(:name => "Duane", :id => nil, :stylist_id => 1)
       expect(test_client.id()).to(eq(nil))
+    end
+  end
+
+  describe('#stylist_id') do
+    it('returns a stylist id') do
+      test_client = Client.new(:name => "Duane", :id => nil, :stylist_id => 1)
+      expect(test_client.stylist_id()).to(eq(1))
     end
   end
 
   describe('.all') do
     it('returns all clients') do
       expect(Client.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a client to an array of clients') do
+      test_client = Client.new(:name => "Duane", :id => nil, :stylist_id => 1)
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same client and stylist_id ID") do
+      client1 = Client.new({:name => "Duane", :id => nil, :stylist_id => 1})
+      client2 = Client.new({:name => "Duane", :id => nil, :stylist_id => 1})
+      expect(client1).to(eq(client2))
     end
   end
 
