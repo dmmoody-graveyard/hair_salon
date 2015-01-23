51b703dc13f021b9e6264a91120ba5f08221b098
Duane M Moody <dmmoody@gmail.com>
2015-01-23 13:26:02 -0800

    add required files and index route

---
+++ app.rb
@@ -0,0 +1,13 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/client")
+require("./lib/stylist")
+require("pg")
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get("/") do
+  @sytlists = Stylist.all()
+  erb(:index)
+end
